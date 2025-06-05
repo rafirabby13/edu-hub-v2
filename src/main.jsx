0cedc763c8b06d135586474e015c2939e12a1e6a
rafi <rafiahmedrabby282@gmail.com>
2025-06-06 02:29:39 +0600

    update the ai promt

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { StrictMode } from 'react'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router';
-import { router } from './Layout/Layout.jsx';
-import AuthProvider from './providers/AuthProvider.jsx';
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router";
+import { router } from "./Layout/Layout.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <AuthProvider>
-    <RouterProvider router={router}>
-     <App />
-   </RouterProvider>
-   </AuthProvider>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+    </AuthProvider>
+  </StrictMode>
+);
